c6a28cff0b272d01a9df83a9e57c8a26a4488169
Shakil Ahmed <shakil.ahmed@corp.eastlink.ca>
2021-10-18 15:36:13 -0300

    Change default logging level to @jitsusama/logging-js library

--- src/index.js
+++ src/index.js
@@ -16,10 +16,8 @@ class Environment {
    */
   constructor(options) {
     const { environment = process.env, logs } = options || {};
-    const {
-      layer = "config-env",
-      level = environment.LOGGING_LEVEL || "silent",
-    } = logs || {};
+    const { layer = "config-env", level = environment.LOGGING_LEVEL } =
+      logs || {};
 
     this.environment = environment;
     this.log = logging.getLogger(layer, level);
